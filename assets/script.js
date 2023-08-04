baccce8da5b5d27b99d7f443e2db6b4939d15943
Fancy Nancy <ramireznancy.686@gmail.com>
2023-08-04 14:49:59 -0600

    fixed path errors

--- assets/script.js
+++
@@ -1,5 +0,0 @@
-function toggleMenu() {
-  const mobileLinks = document.querySelector(".mobile-links");
-  mobileLinks.style.display =
-    mobileLinks.style.display === "block" ? "none" : "block";
-}
