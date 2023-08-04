6e14743c60394592dbfb2cd5ff90094c88a644c1
Fancy Nancy <ramireznancy.686@gmail.com>
2023-08-04 00:00:13 -0600

    updated portfolio

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+  const mobileLinks = document.querySelector(".mobile-links");
+  mobileLinks.style.display =
+    mobileLinks.style.display === "block" ? "none" : "block";
+}
